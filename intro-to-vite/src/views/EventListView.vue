6f1d3d7e10c54e9b0176172e099a1066b0e8d947
mpani03 <milkmilkxciii@gmail.com>
2024-08-02 01:38:02 +0700

    15.3 & 15.4

---
+++ intro-to-vite/src/views/EventListView.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue';
+import CategoryCard from '@/components/CategoryCard.vue'
+import type { Event } from '@/type';
+import {ref,onMounted} from 'vue'
+import axios from 'axios'
+
+const events = ref<Event[] | null>(null)
+
+onMounted(() => {
+  axios
+    .get('https://my-json-server.typicode.com/pppaniii/dbLab2/events')
+    .then((response) => {
+      events.value = response.data
+    })
+    .catch((error) => {
+      console.error('There was an error!', error)
+    })
+})
+</script>
+
+<template>
+  <h1>Event For Good</h1>
+  <!-- new element -->
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+  <div class="category">
+    <CategoryCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.category {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: right;
+}
+</style>
