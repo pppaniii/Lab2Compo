2eaf1794283acdaa908e45536362ea79073d5395
mpani03 <milkmilkxciii@gmail.com>
2024-08-02 00:20:12 +0700

    8.4

--- intro-to-vite/src/components/EventCard.vue
+++ intro-to-vite/src/components/EventCard.vue
@@ -1,24 +1,23 @@
 <script setup lang="ts">
-import {ref} from 'vue'
-// defineProps<{
-//   msg: string
-// }>()
-const event = ref({
-  id: 5928101,
-  category: 'animal welfare',
-  title: 'Cat Adoption Day',
-  description: 'Find your new feline friend at this event.',
-  location: 'Meaw Town',
-  date: 'January 28, 2022',
-  time: '12:00',
-  petsAllowed: true,
-  organizer: 'Kat Laydee'
-})
+import { ref } from 'vue'
+import type {Event} from '@/type';
+defineProps < {event: Event}>()
+// const event = ref({
+//   id: 5928101,
+//   category: 'animal welfare',
+//   title: 'Cat Adoption Day',
+//   description: 'Fine your new feline friend at this event.',
+//   location: 'Meow Town',
+//   date: 'January 28, 2022',
+//   time: '12:00',
+//   petsAllowed: true,
+//   organizer: 'Kat Laydee'
+// })
 </script>
 
 <template>
   <div class="event-class">
-    <div class="event-class">
+    <div class="event-card">
       <h2>{{ event.title }}</h2>
       <span>@{{ event.time }} on {{ event.date }}</span>
     </div>
@@ -26,8 +25,7 @@ const event = ref({
 </template>
 
 <style scoped>
-
-.event-card{
+.event-card {
   padding: 20px;
   width: 250px;
   cursor: pointer;
@@ -37,7 +35,7 @@ const event = ref({
 
 .event-card:hover {
   transform: scale(1.01);
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0, 0.2);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
 }
-
 </style>
+
